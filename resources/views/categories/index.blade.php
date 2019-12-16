88a110e7307924589403cb5da4035a383e73c287
Laetitia Do Nascimento <laetitia@ctrlweb.ca>
2019-12-16 17:26:32 -0500

    Create post and category migration, auth scaffolding, handle create store index category

---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+
+
+@section('content')
+
+<div class="d-flex justify-content-end mb-2">
+    <a href="{{ route('categories.create') }}" class="btn btn-success">Add categories</a>
+</div>
+
+
+<div class="card card-default">
+    <div class="card-header">
+        Categories
+    </div>
+    <div class="card-body">
+        <table class="table">
+            <thead>
+                <th><strong>Name</strong></th>
+            </thead>
+            <tbody>
+                @foreach ($categories as $category)
+                <tr>
+                    <td>
+                        {{ $category->name }}
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+
+@endsection
