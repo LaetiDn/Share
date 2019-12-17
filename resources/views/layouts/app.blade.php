9b933a0ccead2bdfdba79e742341c8b6c035a8b3
Laetitia Do Nascimento <laetitia@ctrlweb.ca>
2019-12-17 12:40:12 -0500

    add trix editor to post

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,8 +10,6 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -19,6 +17,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @yield('css')
 </head>
 
 <body>
@@ -112,6 +111,9 @@
             @endauth
         </main>
     </div>
+   <!-- Scripts -->
+   <script src="{{ asset('js/app.js') }}" ></script>
+
     @yield('scripts')
 
 </html>
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -19,29 +19,39 @@
             </ul>
         </div>
         @endif
-        <form action="{{ isset($post) ? route('posts.update', $post->id) : route('posts.store') }}" method="POST" enctype="multipart/form-data">
+        <form action="{{ isset($post) ? route('posts.update', $post->id) : route('posts.store') }}" method="POST"
+            enctype="multipart/form-data">
             @csrf
             @if(isset($post))
-                @method('PUT')
+            @method('PUT')
             @endif
             <div class="form-group">
-                <input type="text" class="form-control" name="title" id="title" value="{{ isset($post) ? $post->title : '' }}" placeholder="{{ isset($post) ? $post->name : 'Title' }}">
+                <input type="text" class="form-control" name="title" id="title"
+                    value="{{ isset($post) ? $post->title : '' }}"
+                    placeholder="{{ isset($post) ? $post->name : 'Title' }}">
             </div>
 
             <div class="form-group">
-                <textarea class="form-control" name="description" id="description" cols="5" rows="5" value="{{ isset($post) ? $post->description : '' }}" placeholder="{{ isset($post) ? $post->description : 'Description' }}"></textarea>
+                <textarea class="form-control" name="description" id="description" cols="5" rows="5"
+                    value="{{ isset($post) ? $post->description : '' }}"
+                    placeholder="{{ isset($post) ? $post->description : 'Description' }}"></textarea>
             </div>
 
             <div class="form-group">
-                <textarea class="form-control" name="content" id="content" cols="5" rows="5" value="{{ isset($post) ? $post->content : '' }}" placeholder="{{ isset($post) ? $post->content : 'Content' }}"></textarea>
+                <input id="content" type="hidden" name="content">
+                <trix-editor input="content"></trix-editor>
             </div>
 
             <div class="form-group">
-                <input type="text" class="form-control" name="published_at" id="published_at" value="{{ isset($post) ? $post->published_at : '' }}" placeholder="{{ isset($post) ? $post->published_at : 'Published at' }}">
+                <input type="text" class="form-control" name="published_at" id="published_at"
+                    value="{{ isset($post) ? $post->published_at : '' }}"
+                    placeholder="{{ isset($post) ? $post->published_at : 'Published at' }}">
             </div>
 
             <div class="form-group">
-                <input type="file" class="form-control" name="image" id="image" value="{{ isset($post) ? $post->published_at : '' }}" placeholder="{{ isset($post) ? $post->published_at : 'Image' }}">
+                <input type="file" class="form-control" name="image" id="image"
+                    value="{{ isset($post) ? $post->published_at : '' }}"
+                    placeholder="{{ isset($post) ? $post->published_at : 'Image' }}">
             </div>
 
             <div class="class-group">
@@ -53,4 +63,10 @@
     </div>
 </div>
 
+@endsection
+@section('scripts')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.2.1/trix.js"></script>
+@endsection
+@section('css')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/trix/1.2.1/trix.css">
 @endsection
